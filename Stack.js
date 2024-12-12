a1418618e95a9cd4ba0fe16a3668f577acf329b1
Naman Goyal <namangoyal.com@gmail.com>
2024-12-12 10:46:36 +0530

    Implementation of Stack using Arrays

---
+++ Stack.js
@@ -0,0 +1,48 @@
+class Stack{
+    constructor(){
+        this.array = [];
+    }
+
+    // To Push
+    push(data){
+        this.array.push(data);
+    }
+    // To Display the element
+    display(){
+        console.log(this.array);
+    }
+    // To Display the top element 
+    peek(){
+        console.log(this.array[this.array.length-1]);
+    }
+    // To pop the top the element
+    pop(){
+        this.array.pop();
+    }
+    // To check the stack is empty
+    isEmpty(){
+        if(this.array.length == 0){
+            console.log(true);
+        }
+        else{
+            console.log(false);
+        }
+        
+    }
+    // To check the size of the stack
+    size(){
+        console.log(this.array.length);
+    }
+}
+
+const myStack = new Stack();
+myStack.push(5);
+myStack.push(6);
+myStack.push(7);
+myStack.push(8);
+myStack.push(9);
+myStack.pop();
+myStack.peek();
+myStack.isEmpty();
+myStack.size();
+myStack.display();
